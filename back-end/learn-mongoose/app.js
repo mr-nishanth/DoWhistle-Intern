979d0938be12abe62ca1bd939cc3cde57729ac43
Nishanth M <innocentnishanth@gmail.com>
2022-12-18 08:34:48 +0530

    line divided added

--- back-end/learn-mongoose/app.js
+++ back-end/learn-mongoose/app.js
@@ -11,23 +11,26 @@ db.DBConn()
 
 // Different way to create or insert the documents
 
+//================================================================
 // const user = new User({
 //     name: "Naveen",
 //     age: 22
 // })
 
+//================================================================
 // user.save().then(() => {
 //     console.log("USer inserted")
 // }).catch(err => console.log(err))
 
 
+//================================================================
 // const saveUser = async () => {
 //     const result = await user.save()
 //     console.log(result)
 // }
 // saveUser()
 
-
+//================================================================
 // const insertUserAndSave = async () => {
 //     const user = await User.create({
 //         name: "Vijay Kumar",
@@ -37,18 +40,49 @@ db.DBConn()
 // }
 // insertUserAndSave()
 
-const insertUserAndChangeTheName = async () => {
-    const user = await User.create({
-        name: "Vijay Kumar",
-        age: 21
-    })
-    user.name = "Raja"
 
-    await user.save() // save method is import
+//================================================================
+// const insertUserAndChangeTheName = async () => {
+//     const user = await User.create({
+//         name: "Vijay Kumar",
+//         age: 21
+//     })
+//     user.name = "Raja"
 
-    console.log(user)
-}
-insertUserAndChangeTheName()
+//     await user.save() // save method is import
+
+//     console.log(user)
+// }
+// insertUserAndChangeTheName()
+
+//================================================================
+
+
+
+
+//================================================================
+
+
+
+
+//================================================================
+
+
+
+
+//================================================================
+
+
+
+
+//================================================================
+
+
+
+//================================================================
+
+
+
 
 
 app.listen(PORT, () => {
