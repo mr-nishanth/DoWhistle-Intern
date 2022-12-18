0fa58d50366c64d2546b811eb73f0f894908199d
Nishanth M <innocentnishanth@gmail.com>
2022-12-18 08:33:27 +0530

    create schema and insert the document

--- back-end/learn-mongoose/app.js
+++ back-end/learn-mongoose/app.js
@@ -1,11 +1,54 @@
 require("dotenv").config()
 const Express = require('express');
 const db = require("./config/db")
+const User = require("./model/user.model")
 const app = Express();
 
 const PORT = process.env.PORT || 3500
 
 db.DBConn()
+
+
+// Different way to create or insert the documents
+
+// const user = new User({
+//     name: "Naveen",
+//     age: 22
+// })
+
+// user.save().then(() => {
+//     console.log("USer inserted")
+// }).catch(err => console.log(err))
+
+
+// const saveUser = async () => {
+//     const result = await user.save()
+//     console.log(result)
+// }
+// saveUser()
+
+
+// const insertUserAndSave = async () => {
+//     const user = await User.create({
+//         name: "Vijay Kumar",
+//         age: 21
+//     })
+//     console.log(user)
+// }
+// insertUserAndSave()
+
+const insertUserAndChangeTheName = async () => {
+    const user = await User.create({
+        name: "Vijay Kumar",
+        age: 21
+    })
+    user.name = "Raja"
+
+    await user.save() // save method is import
+
+    console.log(user)
+}
+insertUserAndChangeTheName()
 
 
 app.listen(PORT, () => {
